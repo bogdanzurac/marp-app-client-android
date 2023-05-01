9ace68624c7058430f66ddeb46e1b481d49944b5
Bogdan Zurac <bogdan.zurac@gmail.com>
2023-05-01 15:37:58 +0300

    Added feature toggles for all 5 features

---
+++ feature-movies-domain/src/main/kotlin/dev/bogdanzurac/marp/feature/movies/domain/features.kt
@@ -0,0 +1,5 @@
+package dev.bogdanzurac.marp.feature.movies.domain
+
+import dev.bogdanzurac.marp.core.feature.KeyFeature
+
+object MoviesFeature : KeyFeature("movies")
---
+++ feature-weather-domain/src/main/kotlin/dev/bogdanzurac/marp/feature/weather/domain/features.kt
@@ -0,0 +1,5 @@
+package dev.bogdanzurac.marp.feature.weather.domain
+
+import dev.bogdanzurac.marp.core.feature.KeyFeature
+
+object WeatherFeature : KeyFeature("weather")
